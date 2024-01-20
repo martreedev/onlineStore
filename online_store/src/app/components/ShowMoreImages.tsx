f8679f62c422a9c3adea794b563f3c58105300e9
martreedev <130103050+martreedev@users.noreply.github.com>
2024-01-20 01:40:26 -0600

    Added: Remaining Functionality to Image viewer
    Added:
    -Scrolling pictures with left and right arrows
    -

--- online_store/src/app/components/ShowMoreImages.tsx
+++ online_store/src/app/components/ShowMoreImages.tsx
@@ -2,6 +2,8 @@
 import XMarkImage from '@/app/images/XMark.svg'
 import { useEffect, useState } from 'react';
 import placeholderImage from '@/app/images/ImageLoading.png'
+import RightArrowImage from '@/app/images/RightArrow.svg'
+import LeftArrowImage from '@/app/images/LeftArrow.svg'
 
 interface ShowMoreImagesProps
 {
@@ -42,30 +44,55 @@ function ShowMoreImages(props:ShowMoreImagesProps){
         }
     }
 
-    
+    const IncrementSelectedPircture = (direction:string)=>{
+        if (direction == "right"){
+            SelectImageOnClick(SelectedButton +1)
+        }else if (direction == "left"){
+            SelectImageOnClick(SelectedButton -1)
+        }
+    }
 
     return (
         <div>
             <div style={{height:"100%", width:"100%"}} className=" bg-white z-50 fixed">
                 <ExitButton/>
 
+
+            
+
+
                 <div className='flex justify-center pt-10 '>
-                    <img style={{width:"44.813rem"}} className='shadow-lg rounded-lg' src={Images? SelectedImage : ""}></img>
+                    <img style={{width:"44.813rem"}} className='shadow-lg rounded-lg' src={Images? SelectedImage : placeholderImage.src}></img>
                 </div>
 
-                <div className=' flex justify-center gap-8 mt-8'>
+                <div className=' flex justify-center items-center gap-8 mt-8'>
+                    
+                    <button disabled={SelectedButton == 0? true: false} onClick={()=>{IncrementSelectedPircture("left")}} className='p-6'>
+                        <div className='bg-gray-100 w-11 h-11 rounded-full flex justify-center hover:bg-gray-300'>
+                            <img className='w-8' alt='exit window' src={LeftArrowImage.src}></img>
+                        </div>
+                    </button>
+                    
+
                     {Images?.map((image,key)=>{
                         let style = "#D1D5DB"
                         if (SelectedButton == key){
                             style = "#DC2626"
                         }
                         return(
-                        <button style={{borderColor: style}} onClick={()=>{SelectImageOnClick(key)}} key={key} className='w-20 border-2 shadow-xl'>
+                        <button style={{borderColor: style}} onClick={()=>{SelectImageOnClick(key)}} key={key} className='w-20 h-20 border-2 shadow-xl'>
                             <img src={image}></img>
                         </button>
 
                         )
                     })}
+                    
+                    <button disabled={Images && SelectedButton ==Images.length-1? true: false} onClick={()=>{IncrementSelectedPircture("right")}} className='p-6'>
+                        <div className='bg-gray-100 w-11 h-11 rounded-full flex justify-center hover:bg-gray-300'>
+                            <img className='w-8' alt='exit window' src={RightArrowImage.src}></img>
+                        </div>
+                    </button>
+                    
                 </div>
             </div>
         </div>
