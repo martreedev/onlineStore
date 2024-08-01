1b702c076206a9138792fa1dc92511033fe811f7
martreedev <mariusztweb@gmail.com>
2024-07-31 19:02:52 -0500

    Update CartInformationContainer.tsx

--- online_store/src/app/cart/cartComponents/CartInformationContainer.tsx
+++ online_store/src/app/cart/cartComponents/CartInformationContainer.tsx
@@ -1,25 +1,24 @@
 import { FormatPrice } from "@/app/functions/FormatProductPrice";
 
-interface CartInformationContainerProps{
-    ItemCount:number
-    SubTotal:number
+interface CartInformationContainerProps {
+    ItemCount: number
+    SubTotal: number
 }
 
-const CartInformationContainer = (props:CartInformationContainerProps)=>{
-    
-    const displayPrice = FormatPrice(props.SubTotal)
-    let h2DisplayString = `${displayPrice} subtotal - ${props.ItemCount} `   
+const CartInformationContainer = (props: CartInformationContainerProps) => {
 
-    if (props.ItemCount >1){
+    const displayPrice = FormatPrice(props.SubTotal)
+    let h2DisplayString = `${displayPrice} subtotal - ${props.ItemCount} `
+
+    if (props.ItemCount > 1) {
         h2DisplayString += 'items'
-    }else{
+    } else {
         h2DisplayString += 'item'
     }
 
     return (
         <div className="">
-            <h1 className="text-3xl font-bold text-gray-900">Cart</h1>
-            <h2 className="text-gray-600 font-bold text-xl mt-4">{h2DisplayString}</h2>
+
         </div>
     )
 };
